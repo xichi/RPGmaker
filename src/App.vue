84d1fef3e2e5beedb86538c9679b45164280aa16
XiChi <474504964@qq.com>
2019-09-02 23:45:23 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <canvasControl/>
+  </div>
+</template>
+
+<script>
+import canvasControl from './components/canvasControl.vue'
+export default {
+  name: 'app',
+  components: {
+    canvasControl
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/canvasControl.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <canvas id="map"></canvas>
+    </div>
+</template>
+
+<script>
+export default{
+  mounted(){
+    var canvas = document.getElementById("map");
+    var ctx = canvas.getContext("2d");
+    canvas.width = 480;
+    canvas.height = 480;
+ }
+}
+</script>
+
+<style lang="stylus" scoped>
+  #map
+   margin:0 auto 
+   background-color:pink
+</style>
